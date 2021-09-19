25d406f3b7221ac9051b31711231d05f4add9788
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-19 15:51:09 +0530

    [ASHIQ] added : Single and Multi Cast Delegate files

---
+++ MultiCastDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    
+    public class MultiCastDelegate
+    {
+        delegate void DelOp(int a, int b);
+        public static void ImplementDelegate()
+        {
+            //Delegate instantiation
+            DelOp obj = Operation.Add;
+            obj += Operation.Square;
+
+            obj(1, 2);
+            obj(8, 3);
+
+            Console.ReadLine();
+        }
+        public class Operation
+        {
+            public static void Add( int a, int b)
+            {
+                Console.WriteLine(" Addition={0}", a + b);
+
+            }
+            public static void Square(int a, int b)
+            {
+                Console.WriteLine("Multiple={0}", a * b);
+
+            }
+        }
+
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    //Delegate Definition or Declaration
+    public delegate int operation(int a, int b);
+    class Program
+    {
+        //Method that is passes as an Argument
+        //It has same signature as Delagetes
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Welcome to Delegates");
+
+            //Delegate instantiation 
+            operation obj = new operation(Addition);
+
+            //output of SingleDelegate
+            Console.WriteLine("Addition is={0}", obj(23, 27));
+            Console.WriteLine();
+
+            //MultiCastDelegate
+            MultiCastDelegate.ImplementDelegate();
+            Console.Read();
+
+        }
+        
+    }
+}
